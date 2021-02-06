2788d7d7de9d24a46de28bc9c6b008bec7bd776c
IgorIvanov123 <igor.iva2@yandex.ru>
2021-02-06 15:09:04 +0300

    Инициализация проекта

---
+++ src/main/java/com/petProjects/mergePdf/MergePdfApplication.java
@@ -0,0 +1,13 @@
+package com.petProjects.mergePdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergePdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MergePdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petProjects/mergePdf/MergePdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petProjects.mergePdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergePdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
