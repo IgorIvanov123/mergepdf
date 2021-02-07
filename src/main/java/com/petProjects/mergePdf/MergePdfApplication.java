5b1a59f6623a77ebc66af3ffded1b8cad0f9a265
Vladislav Tkachuk <mail@tkachuk.su>
2021-02-07 19:00:21 +0300

    Редизайн главной страницы

--- src/main/java/com/petProjects/mergePdf/MergePdfApplication.java
+++ src/main/java/com/petProjects/mergePdf/MergePdfApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MergePdfApplication {
-
+    
 	public static void main(String[] args) {
 		SpringApplication.run(MergePdfApplication.class, args);
-	}
+	} 
 	
 }
--- src/main/resources/static/scripts/main.controller.js
+++ src/main/resources/static/scripts/main.controller.js
@@ -1,7 +1,5 @@
 angular.module('mergepdfapp').controller('mainController', function mainControllerFunction($scope, $http) {
-    $scope.cats = ['pushok', 'murzik'];
-    $scope.message = "12";
-    $scope.fun = function(){
-        console.log($scope.message);
-    }
+
+    
+
 })
